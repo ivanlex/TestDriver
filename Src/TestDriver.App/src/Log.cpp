78af841b1b6882da84c3fb358d960143a5873124
Kevin Dai <lexdxw@126.com>
2024-05-12 21:46:35 +0800

    Configure the C++ project output and intermediate directory.

---
+++ Src/TestDriver.App/src/Log.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int MultiplyR(int a, int b)
+{
+	return a * b;
+}
---
+++ Src/TestDriver.App/src/TestDriverApp.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int MultiplyR(int a, int b);
+
+int main()
+{
+	std::cout << MultiplyR(1,2) << std::endl;
+	return 0;
+}
