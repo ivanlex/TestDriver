e9560e6d42331fea7937b14f75ddb556e6f0b054
DXW <lexdxw@gmail.com>
2024-06-05 15:26:32 +0800

    Relationships between vertices, buffers, indices.

--- Src/GLWorld.App/AppMain.cpp
+++ Src/GLWorld.App/AppMain.cpp
@@ -5,6 +5,15 @@ float vertices[] = {
 	// first triangle   //color
 	0.5f, 0.5f, 0.0f,   1.f, 0,  0,
 	0.5f, -0.5f, 0.0f,  0,  1.f, 0,
+	-0.5f, 0.5f, 0.0f,  0,   0, 1.f,
+	-0.5f, 0.5f, 0.0f,  0,   0, 1.f
+};
+
+float vertices2[] = {
+	// first triangle   //color
+	0.5f, 0.1f, 0.0f,   1.f, 0,  0,
+	0.5f, -0.5f, 0.0f,  0,  1.f, 0,
+	-0.5f, 0.5f, 0.0f,  0,   0, 1.f,
 	-0.5f, 0.5f, 0.0f,  0,   0, 1.f
 };
 
@@ -34,26 +43,55 @@ int main()
 
 	glfwMakeContextCurrent(window);
 	gladLoadGL(glfwGetProcAddress);
-	glfwSwapInterval(1);
+	glfwSwapInterval(0);
 	setCallbacks(window);
 	glClearColor(0.f, 0.f, 0.f, 1.f);
 
 	Shader defaultShader(DEFAULT_VERTEX, DEFAULT_FRAGMENT);
 
-	GLuint vbo;
+	GLuint vbo, vbo2, vao, vao2, ebo;
+
+	glGenVertexArrays(1, &vao);
+	glGenVertexArrays(1, &vao2);
 	glGenBuffers(1, &vbo);
+	glGenBuffers(1, &vbo2);
+	glGenBuffers(1, &ebo);
+
+	glBindVertexArray(vao);
+	
 	glBindBuffer(GL_ARRAY_BUFFER, vbo);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GL_FLOAT), (void*)NULL);
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GL_FLOAT), (void*)(3 * sizeof(float)));
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GL_FLOAT), (void*)NULL);
 	glEnableVertexAttribArray(0);
+
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GL_FLOAT), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+
+
+	glBindVertexArray(vao2);
+
+	glBindBuffer(GL_ARRAY_BUFFER, vbo2);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices2), vertices2, GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GL_FLOAT), (void*)NULL);
+	glEnableVertexAttribArray(0);
+
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GL_FLOAT), (void*)(3 * sizeof(float)));
 	glEnableVertexAttribArray(1);
 
 	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
+	
 	defaultShader.use();
+	
+	
 
 	while (!glfwWindowShouldClose(window))
 	{
@@ -63,9 +101,19 @@ int main()
 		float xValue = cos(time) / 0.2f;
 		float yValue = cos(time) / 0.3f;
 		float zValue = cos(time) / 0.4f;
+
+		if (tan(xValue + yValue + zValue) > 0)
+		{
+			glBindVertexArray(vao);
+		}
+		else
+		{
+			glBindVertexArray(vao2);
+		}
+
 		defaultShader.setFloat3("vertexColorParam", xValue, yValue, zValue);
 
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
